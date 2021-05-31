65f26395eef3e1ee553a3901201a8fda203ae62b
Krishan K Yadav <aniandp@gmail.com>
2021-06-01 00:47:25 +0530

    port fixed

--- server.js
+++ server.js
@@ -70,6 +70,6 @@ io.on('connection', (socket) => {
   })
 })
 
-const PORT = 3000 || process.env.PORT
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+server.listen(process.env.PORT || 3000, () =>
+  console.log(`Server running on port ${process.env.PORT}`)
+)
